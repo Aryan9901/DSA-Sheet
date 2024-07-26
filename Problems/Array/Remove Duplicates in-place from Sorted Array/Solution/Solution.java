b10035f64ed4802750119f728e1fad4092024616
Aryan9901 <911aaryan@gmail.com>
2024-07-26 22:23:04 +0530

    added main method in problem 2

--- Problems/Array/Remove Duplicates in-place from Sorted Array/Solution/Solution.java
+++ Problems/Array/Remove Duplicates in-place from Sorted Array/Solution/Solution.java
@@ -1,5 +1,5 @@
 class Solution {
-     public int removeDuplicates(int[] nums) {
+     public static int removeDuplicates(int[] nums) {
           int i = 0;
           for (int j = 1; j < nums.length; j++) {
                if (nums[j] != nums[i]) {
@@ -8,5 +8,13 @@ class Solution {
                }
           }
           return i + 1;
+     }
+
+     public static void main(String[] args) {
+          int[] arr = { 1, 2, 2, 3, 3, 3, 3, 4, 5, 5, 6, 7, 7, 12, 12, 12, 13, 18 };
+          int length = removeDuplicates(arr);
+          for (int i = 0; i < length; i++) {
+               System.out.print(arr[i] + " ");
+          }
      }
 }
