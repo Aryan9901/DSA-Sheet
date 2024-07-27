1d6a5f02f75c5478160bdfda4de0b537f78dfc49
Aryan9901 <911aaryan@gmail.com>
2024-07-27 20:32:19 +0530

    pdated optimal solution of left rotate array

--- Problems/Array/Left Rotate an array/Solution/OptimalSolution.java
+++ Problems/Array/Left Rotate an array/Solution/OptimalSolution.java
@@ -1,20 +1,23 @@
 public class OptimalSolution {
-     void leftRotate(int arr[], int d) {
-          arr = reverse(arr, 0, d - 1);
-          arr = reverse(arr, d, arr.length - 1);
-          arr = reverse(arr, 0, arr.length - 1);
+     public void rotate(int[] nums, int k) {
+          // Handle cases where k is greater than the length of the array
+          k = k % nums.length;
+          reverse(nums, 0, nums.length - 1);
+          reverse(nums, 0, k - 1);
+          reverse(nums, k, nums.length - 1);
      }
 
-     int[] reverse(int[] arr, int low, int high) {
-          while (low <= high) {
+     // Reverses elements in the array from index low to high
+     private void reverse(int[] arr, int low, int high) {
+          while (low < high) {
                swap(arr, low, high);
                low++;
                high--;
           }
-          return arr;
      }
 
-     void swap(int[] arr, int f, int s) {
+     // Swaps elements in the array at indices f and s
+     private void swap(int[] arr, int f, int s) {
           int temp = arr[f];
           arr[f] = arr[s];
           arr[s] = temp;
