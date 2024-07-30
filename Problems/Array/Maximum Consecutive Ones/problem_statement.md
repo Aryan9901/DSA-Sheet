81bc89d4421307fde7c86e8051037421169832d6
Aryan9901 <911aaryan@gmail.com>
2024-07-30 18:34:31 +0530

    added problem maximum consective ones in Array

---
+++ Problems/Array/Maximum Consecutive Ones/problem_statement.md
@@ -0,0 +1,25 @@
+## Maximum Consecutive Ones
+
+## Description
+
+Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+## Examples
+
+### Example 1
+
+Input:</br> `nums = [1,1,0,1,1,1]` </br>
+Output:</br> `3` </br>
+Explanation:
+
+The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+
+### Example 2
+
+Input:</br> `nums = [1,0,1,1,0,1]` </br>
+Output:</br> `2` </br>
+
+Constraints:
+
+1 1 <= nums.length <= 105</br>
+2 nums[i] is either 0 or 1. </br>
---
+++ Problems/Array/Union of Two Sorted Arrays/Solution/BetterSolution.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+
+/**
+ * NaiveSolution
+ */
+public class BetterSolution {
+
+    public ArrayList<Integer> findUnionOfArrays(int[] arr1, int[] arr2, int n, int m) {
+        ArrayList<Integer> newArr = new ArrayList<>();
+        int i = 0, j = 0;
+
+        // Traverse both arrays
+        while (i < n && j < m) {
+            // Skip duplicates in arr1
+            while (i > 0 && i < n && arr1[i] == arr1[i - 1])
+                i++;
+            // Skip duplicates in arr2
+            while (j > 0 && j < m && arr2[j] == arr2[j - 1])
+                j++;
+
+            // Check boundaries again after skipping duplicates
+            if (i < n && j < m) {
+                if (arr1[i] < arr2[j]) {
+                    newArr.add(arr1[i]);
+                    i++;
+                } else if (arr1[i] > arr2[j]) {
+                    newArr.add(arr2[j]);
+                    j++;
+                } else {
+                    newArr.add(arr1[i]);
+                    i++;
+                    j++;
+                }
+            }
+        }
+
+        // Store remaining elements of arr1
+        while (i < n) {
+            if (i == 0 || arr1[i] != arr1[i - 1]) {
+                newArr.add(arr1[i]);
+            }
+            i++;
+        }
+
+        // Store remaining elements of arr2
+        while (j < m) {
+            if (j == 0 || arr2[j] != arr2[j - 1]) {
+                newArr.add(arr2[j]);
+            }
+            j++;
+        }
+
+        return newArr;
+    }
+}
