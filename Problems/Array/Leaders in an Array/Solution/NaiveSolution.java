bf5847536d42bbd3694762e72c773fbef4c6dd97
Aryan9901 <911aaryan@gmail.com>
2024-10-17 12:48:01 +0530

    updated input output and naive solution

---
+++ Problems/Array/Leaders in an Array/Solution/NaiveSolution.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+class Solution {
+    public static ArrayList<Integer> leaders(int arr[]) {
+        ArrayList<Integer> leaders = new ArrayList<>();
+
+        boolean flag = true;
+        for (int i = 0; i < arr.length; i++) {
+            flag = true;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                leaders.add(arr[i]);
+            }
+        }
+        return leaders;
+    }
+}
