a96e6698e0ccf70463815c023db9539e43444025
Aryan9901 <911aaryan@gmail.com>
2024-10-17 21:19:08 +0530

    added  input output

---
+++ Problems/Array/Maximum Sum Subarray/Solution/NaiveSolution.java
@@ -0,0 +1,12 @@
+
+/**
+ * NaiveSolution
+ */
+
+import java.util.ArrayList;
+
+public class NaiveSolution {
+    public static ArrayList<Integer> findSubarray(int n, int a[]) {
+        return null;
+    }
+}
