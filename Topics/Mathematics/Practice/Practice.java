6e79d769e7bffbb2d477a78074335ddc13b8bdac
Aryan9901 <911aaryan@gmail.com>
2024-10-18 22:19:22 +0530

    euclids gcd, sieve of eratosthenes problem and isPrime problem practice added

--- Topics/Mathematics/Practice/Practice.java
+++ Topics/Mathematics/Practice/Practice.java
@@ -1,6 +1,7 @@
 package Topics.Mathematics.Practice;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Practice {
 
@@ -31,6 +32,45 @@ public class Practice {
         return rev == n;
     }
 
+    public static boolean isPrime(int n) {
+        if (n == 2 || n == 3)
+            return true;
+        if (n % 2 == 0 || n % 3 == 0)
+            return false;
+
+        for (int i = 5; i * i <= n; i = i + 6) {
+            if ((n % i) == 0 || (n % (i + 2)) == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    public static boolean[] sieve(int n) {
+        boolean[] isPrime = new boolean[n + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = false;
+        isPrime[1] = false;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        return isPrime;
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
         System.out.print("Enter the number: ");
@@ -40,8 +80,30 @@ public class Practice {
         System.out.println("Trailing zeroes of " + n + " is " + trailingZeroes(n));
 
         System.out.print("Enter number to check for palindrome: ");
-        int x = kb.nextInt();
+        n = kb.nextInt();
         System.out
-                .println("the number " + x + " is " + (isPalindrome(x) == true ? "a palindrome" : "not a palindrome"));
+                .println("the number " + n + " is " + (isPalindrome(n) == true ? "a palindrome" : "not a palindrome"));
+
+        System.out.print("Enter number to check for prime: ");
+        n = kb.nextInt();
+        System.out.println("The number " + n + " is " + (isPrime(n) == true ? "a prime number" : "not a prime number"));
+
+        System.out.print("Enter number to print prime numbers: ");
+        n = kb.nextInt();
+        boolean[] primes = sieve(n);
+        System.out.println("all prime nuumbers are: ");
+        for (int i = 1; i < primes.length; i++) {
+            if (primes[i]) {
+                System.out.print(i + ", ");
+            }
+        }
+        System.out.println();
+
+        System.out.print("Enter number a: ");
+        int a = kb.nextInt();
+        System.out.print("Enter number b: ");
+        int b = kb.nextInt();
+
+        System.out.println("GCD of " + a + " & " + b + " is " + gcd(a, b));
     }
 }
