a6941ebec99b0328b19056db67c775d18d00c3ea
Aryan9901 <911aaryan@gmail.com>
2024-10-19 16:04:29 +0530

    added naive solution to isPalindrome string problem

---
+++ Problems/Recurrsion/Palindrome String/Solution/NaiveSolution.java
@@ -0,0 +1,17 @@
+/**
+ * NaiveSolution
+ */
+public class NaiveSolution {
+
+    boolean isPalindrome(String S) {
+        return isPalindromeString(S, 0, S.length() - 1);
+    }
+
+    boolean isPalindromeString(String s, int l, int r) {
+        if (l >= r)
+            return true;
+
+        return ((s.charAt(l) == s.charAt(r)) && isPalindromeString(s, ++l, --r));
+    }
+
+}
