959f0621ecbb7a8a80a7c49d5ec19b200b991a60
Aryan9901 <911aaryan@gmail.com>
2024-10-20 14:46:08 +0530

    added naive and optimal solution for permutations of string problem

---
+++ Problems/Recurrsion/Permutations of a string/Solution/NaiveSolution.java
@@ -0,0 +1,44 @@
+import java.util.Set;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * NaiveSolution
+ */
+public class NaiveSolution {
+    public List<String> find_permutation(String s) {
+        List<String> permutations = new ArrayList<>();
+        StringBuilder sb = new StringBuilder(s);
+        permute(sb, 0, permutations);
+        Collections.sort(permutations);
+        return permutations;
+    }
+
+    public void permute(StringBuilder str, int i, List<String> perm) {
+
+        if (i == str.length() - 1) {
+            perm.add(str.toString());
+            return;
+        }
+
+        Set<Character> seen = new HashSet<>();
+        for (int j = i; j < str.length(); j++) {
+            if (seen.contains(str.charAt(j))) {
+                continue;
+            }
+
+            seen.add(str.charAt(j));
+            swap(str, i, j);
+            permute(str, i + 1, perm);
+            swap(str, i, j);
+        }
+    }
+
+    public void swap(StringBuilder sb, int i, int j) {
+        char temp = sb.charAt(i);
+        sb.setCharAt(i, sb.charAt(j));
+        sb.setCharAt(j, temp);
+    }
+}
